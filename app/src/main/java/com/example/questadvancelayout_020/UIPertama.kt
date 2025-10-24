2e5772a46fe713c9ed1c5a0244175756ffc9b6b9
wildanindi2@gmail.com <wildanindi2@gmail.com>
2025-10-24 16:23:46 +0700

    Menambah box untuk text copyright di bagian bawah

--- app/src/main/java/com/example/questadvancelayout_020/UIPertama.kt
+++ app/src/main/java/com/example/questadvancelayout_020/UIPertama.kt
@@ -89,6 +89,16 @@ fun ActivitasPertama(modifier: Modifier){
             }
 
         }
-
+        Box(
+            modifier = Modifier
+                .fillMaxSize()
+        ){
+            Text(
+                text = stringResource(id = R.string.copy),
+                modifier = Modifier
+                    .align(Alignment.BottomCenter)
+                    .padding(bottom = 50.dp)
+            )
+        }
     }
 }
