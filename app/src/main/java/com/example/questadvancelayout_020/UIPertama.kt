2b6de5e85328204a688195d040da310486a12874
wildanindi2@gmail.com <wildanindi2@gmail.com>
2025-10-24 16:11:13 +0700

    Menambah text univ yang di panggil dari resource value/string.xml

--- app/src/main/java/com/example/questadvancelayout_020/UIPertama.kt
+++ app/src/main/java/com/example/questadvancelayout_020/UIPertama.kt
@@ -29,20 +29,23 @@ import androidx.compose.ui.unit.sp
 
 
 @Composable
-fun ActivitasPertama(modifier: Modifier) {
-    Column(
-        modifier = Modifier
-            .padding(top = 100.dp)
-            .fillMaxSize(),
+fun ActivitasPertama(modifier: Modifier){
+    Column (modifier = Modifier
+        .padding(top = 100.dp)
+        .fillMaxSize(),
         horizontalAlignment = Alignment.CenterHorizontally
-    ) {
+    ){
         Text(
             text = stringResource(id = R.string.prodi),
             fontSize = 30.sp,
             fontWeight = FontWeight.Bold
         )
 
+        Text(
+            text = stringResource(id = R.string.univ),
+            fontSize = 18.sp
+        )
+
 
     }
-
 }
