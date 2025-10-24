0e18e47a47a851fcb3133358b3e4175d468b10b5
wildanindi2@gmail.com <wildanindi2@gmail.com>
2025-10-24 16:24:39 +0700

    Mengubah Greeting pada MainActivity.kt untuk menjalankan aplikasi

--- app/src/main/java/com/example/questadvancelayout_020/MainActivity.kt
+++ app/src/main/java/com/example/questadvancelayout_020/MainActivity.kt
@@ -20,8 +20,8 @@ class MainActivity : ComponentActivity() {
         setContent {
             QuestAdvanceLayout_020Theme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
+                    ActivitasPertama(
+
                         modifier = Modifier.padding(innerPadding)
                     )
                 }
