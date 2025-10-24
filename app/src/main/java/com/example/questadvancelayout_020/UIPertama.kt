eaaa18bf6237a0338fcf9dc9cfa44e75c9855fa7
wildanindi2@gmail.com <wildanindi2@gmail.com>
2025-10-24 16:07:56 +0700

    Menambah function ActivitasPertama sekaligus mengatur ukuran Column menggunakan padding dan fillMaxSize

--- app/src/main/java/com/example/questadvancelayout_020/UIPertama.kt
+++ app/src/main/java/com/example/questadvancelayout_020/UIPertama.kt
@@ -1,2 +1,40 @@
 package com.example.questadvancelayout_020
 
+import android.widget.Space
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.painter.Painter
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+
+
+@Composable
+fun ActivitasPertama(modifier: Modifier){
+    Column (modifier = Modifier
+        .padding(top = 100.dp)
+        .fillMaxSize(),
+        horizontalAlignment = Alignment.CenterHorizontally
+    ){
+
+    }
+}
