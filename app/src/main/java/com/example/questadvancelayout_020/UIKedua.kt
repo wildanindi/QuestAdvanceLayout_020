8a4f75ea56196df5e913014a01d374ce64cfc49a
wildanindi2@gmail.com <wildanindi2@gmail.com>
2025-10-25 12:43:57 +0700

    Menambahkan card untuk container LinkedI

--- app/src/main/java/com/example/questadvancelayout_020/UIKedua.kt
+++ app/src/main/java/com/example/questadvancelayout_020/UIKedua.kt
@@ -206,6 +206,50 @@ fun ProfileWildan(modifier: Modifier) {
 
         }
 
+        Spacer(modifier = Modifier.height(16.dp))
+
+        Card(
+            modifier = Modifier
+                .height(height = 70.dp)
+                .fillMaxWidth(fraction = 0.8f)
+                .padding( start = 16.dp, end = 16.dp),
+            shape = RoundedCornerShape(100.dp),
+            colors = CardDefaults.cardColors(
+                containerColor = colorResource(id = R.color.UntukCard)
+            )
+        ) {
+            Row(
+                verticalAlignment = Alignment.CenterVertically,
+                modifier = Modifier
+                    .padding(all = 10.dp)
+                    .fillMaxSize()
+            ) {
+                Image(
+                    painter = painterResource(id = R.drawable.linkedin),
+                    contentDescription = null,
+                    modifier = Modifier
+                        .size(size = 70.dp)
+                        .padding(all = 1.dp)
+
+                )
+                Spacer(modifier = Modifier.width(16.dp))
+                Column () {
+                    Text(
+                        text = stringResource(id = R.string.link),
+                        fontSize = 15.sp,
+                        fontFamily = FontFamily.Serif,
+                        color = Color.Black,
+                        modifier = Modifier.padding(all = 15.dp)
+                    )
+
+                }
+
+
+            }
+
+
+
+        }
 
     }
 
